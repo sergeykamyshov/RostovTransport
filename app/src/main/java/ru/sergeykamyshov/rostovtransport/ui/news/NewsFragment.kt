f319a952faf0634cbf63d36da05f9662eb16e9b6
sergeykamyshov <kamishovsergey@gmail.com>
2018-06-03 22:26:08 +0300

    Added colors and image for DrawerLayout

--- app/src/main/java/ru/sergeykamyshov/rostovtransport/ui/news/NewsFragment.kt
+++ app/src/main/java/ru/sergeykamyshov/rostovtransport/ui/news/NewsFragment.kt
@@ -22,6 +22,7 @@ class NewsFragment : Fragment(), MvpView {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_news, container, false)
 
+        // TODO: понять почему не меняется заголовок
         activity?.actionBar?.title = activity?.resources?.getString(R.string.title_news)
 
         mPresenter = NewsPresenter()
