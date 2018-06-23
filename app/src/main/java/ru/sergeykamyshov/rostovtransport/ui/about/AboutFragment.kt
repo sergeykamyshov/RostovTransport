889ec8fa1970a072007239fa5a9f6bdd4c9cd937
sergeykamyshov <kamishovsergey@gmail.com>
2018-06-23 14:10:19 +0300

    Changed the logic of loading AboutFragment

--- app/src/main/java/ru/sergeykamyshov/rostovtransport/ui/about/AboutFragment.kt
+++ app/src/main/java/ru/sergeykamyshov/rostovtransport/ui/about/AboutFragment.kt
@@ -3,6 +3,7 @@ package ru.sergeykamyshov.rostovtransport.ui.about
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.support.v7.widget.CardView
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
@@ -27,8 +28,12 @@ class AboutFragment : BaseFragment() {
         setActionBarTitle(R.string.title_about)
 
         val progressBar = view.findViewById<ProgressBar>(R.id.about_progress)
+        val shortDescCard = view.findViewById<CardView>(R.id.cv_short_desc)
         val shortDesc = view.findViewById<TextView>(R.id.tv_short_desc)
+        val importantInfoCard = view.findViewById<CardView>(R.id.cv_important)
         val importantInfo = view.findViewById<TextView>(R.id.tv_important)
+        val contactsCard = view.findViewById<CardView>(R.id.cv_contacts)
+        val fullDescCard = view.findViewById<CardView>(R.id.cv_full_desc)
         val fullDesc = view.findViewById<TextView>(R.id.tv_full_desc)
 
         val recycler = view.findViewById<RecyclerView>(R.id.contacts_recycler)
@@ -42,12 +47,18 @@ class AboutFragment : BaseFragment() {
         val liveData = viewModel.getData()
         liveData.observe(this, Observer {
             shortDesc.text = it?.shortDescription
+            shortDescCard.visibility = View.VISIBLE
+
             importantInfo.text = it?.importantInfo
-            fullDesc.text = it?.fullDescription
+            importantInfoCard.visibility = View.VISIBLE
 
             if (it?.contacts != null && it.contacts.isNotEmpty()) {
                 adapter.updateData(it.contacts)
+                contactsCard.visibility = View.VISIBLE
             }
+
+            fullDesc.text = it?.fullDescription
+            fullDescCard.visibility = View.VISIBLE
 
             progressBar.visibility = View.GONE
         })
