4a34a37449ce3716fe93fd7d6ad51ebad833f4ab
sergeykamyshov <kamishovsergey@gmail.com>
2018-06-29 21:41:03 +0300

    Created simple map example for buy list

--- app/src/main/java/ru/sergeykamyshov/rostovtransport/ui/card/buy/map/CardBuyMapActivity.kt
+++ app/src/main/java/ru/sergeykamyshov/rostovtransport/ui/card/buy/map/CardBuyMapActivity.kt
@@ -1,7 +1,39 @@
 package ru.sergeykamyshov.rostovtransport.ui.card.buy.map
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import ru.sergeykamyshov.rostovtransport.R
 
-class CardBuyMapActivity : AppCompatActivity() {
+class CardBuyMapActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_card_buy_map)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map_card_buy) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            android.R.id.home -> finish()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap?) {
+        val latLng = LatLng(47.224337, 39.629944)
+        googleMap?.addMarker(MarkerOptions().position(latLng).title("67 bus ))"))
+        googleMap?.moveCamera(CameraUpdateFactory.newLatLng(latLng))
+    }
 
 }
