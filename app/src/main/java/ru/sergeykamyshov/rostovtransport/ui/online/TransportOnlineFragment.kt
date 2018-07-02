ea157f55f4b614d7102779e1f4de600acd6a39f6
sergeykamyshov <kamishovsergey@gmail.com>
2018-07-02 22:13:01 +0300

    Changed Service to ViewModel, 'cause we need work only for one fragment

--- app/src/main/java/ru/sergeykamyshov/rostovtransport/ui/online/TransportOnlineFragment.kt
+++ app/src/main/java/ru/sergeykamyshov/rostovtransport/ui/online/TransportOnlineFragment.kt
@@ -1,12 +1,8 @@
 package ru.sergeykamyshov.rostovtransport.ui.online
 
-import android.content.ComponentName
-import android.content.Context
-import android.content.Intent
-import android.content.ServiceConnection
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
-import android.os.IBinder
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -15,32 +11,19 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 import ru.sergeykamyshov.rostovtransport.R
-import ru.sergeykamyshov.rostovtransport.data.network.model.online.Transport
-import ru.sergeykamyshov.rostovtransport.service.TransportOnlineService
+import ru.sergeykamyshov.rostovtransport.data.network.model.online.TransportOnline
 import ru.sergeykamyshov.rostovtransport.ui.base.BaseFragment
 import ru.sergeykamyshov.rostovtransport.utils.Const
 
 class TransportOnlineFragment : BaseFragment(), OnMapReadyCallback {
 
     private var mGoogleMap: GoogleMap? = null
-    private var markers: MutableList<Marker?> = mutableListOf()
-    private lateinit var allTransports: List<Transport.Item>
-
-    // Сервис
-    lateinit var mService: TransportOnlineService
-    // Признак подключения сервиса
-    private var mBound = false
+    private lateinit var mViewModel: TransportOnlineViewModel
 
     companion object {
         fun newInstance() = TransportOnlineFragment()
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        // Запускаем Сервис для обновления данных о передвижении
-//        activity?.startService(Intent(activity, TransportOnlineService::class.java))
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -51,44 +34,33 @@ class TransportOnlineFragment : BaseFragment(), OnMapReadyCallback {
         val mapFragment = childFragmentManager.findFragmentById(R.id.map_transport_online) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
-//        val viewModel = ViewModelProviders.of(activity!!).get(TransportOnlineViewModel::class.java)
-//        val liveData = viewModel.getData()
-//        liveData.observe(this, Observer {
-//            if (it != null) {
-//                allTransports = it
-//            }
-//        })
-//        viewModel.loadData()
+        mViewModel = ViewModelProviders.of(this).get(TransportOnlineViewModel::class.java)
+        // Получаем список транспорта
+        mViewModel.getTransportList().observe(this, Observer {
+            // TODO: Сохранить список транспорта для запроса
+        })
+        mViewModel.loadTransportList()
+
+        // Получаем список транспорта, который находится онлайн
+        mViewModel.getTransportOnline().observe(this, Observer {
+            if (it != null) {
+                updateTransportOnlineOnMap(it)
+            }
+        })
 
         return view
     }
 
     override fun onStart() {
         super.onStart()
-        val intent = Intent(activity, TransportOnlineService::class.java)
-        activity?.bindService(intent, mConnection, Context.BIND_AUTO_CREATE)
+        mViewModel.setRunning(true)
+        // TODO: Убрать заглушку
+        mViewModel.loadTransportOnline("2_67")
     }
 
     override fun onStop() {
         super.onStop()
-        if (mBound) {
-            activity?.unbindService(mConnection)
-            mBound = false
-        }
-    }
-
-    private var mConnection = object : ServiceConnection {
-        override fun onServiceDisconnected(name: ComponentName?) {
-            mBound = false
-            Log.i("TransportOnlineFragment", "Service disconnected")
-        }
-
-        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
-            val binder = service as TransportOnlineService.LocalBinder
-            mService = binder.getService()
-            mBound = true
-            Log.i("TransportOnlineFragment", "Service connected")
-        }
+        mViewModel.setRunning(false)
     }
 
     override fun onMapReady(googleMap: GoogleMap?) {
@@ -96,37 +68,21 @@ class TransportOnlineFragment : BaseFragment(), OnMapReadyCallback {
         mGoogleMap?.uiSettings?.isZoomControlsEnabled = true
         mGoogleMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(
                 LatLng(Const.RND_LATITUDE, Const.RND_LONGITUDE), 12F))
+    }
 
-//        Thread(Runnable {
-//            val restService = App.createOnlineRestService()
-//
-//            var x = 0
-//            while (x < 30) {
-//                val call = restService.getTransportByName("2_67")
-//                call.enqueue(object : Callback<List<TransportOnline>> {
-//                    override fun onFailure(call: Call<List<TransportOnline>>?, t: Throwable?) {
-//                        Log.i("TransportOnlineFragment", "Error when try to get transport online: $t")
-//                    }
-//
-//                    override fun onResponse(call: Call<List<TransportOnline>>?, response: Response<List<TransportOnline>>?) {
-//                        if (response?.code() == 200) {
-//                            val list = response.body()
-//
-//                            activity?.runOnUiThread {
-//                                mGoogleMap?.clear()
-//                                list?.forEach {
-//                                    val lat = "${it.lat.substring(0, 2)}.${it.lat.substring(2)}"
-//                                    val lon = "${it.lon.substring(0, 2)}.${it.lon.substring(2)}"
-//                                    mGoogleMap?.addMarker(MarkerOptions().position(LatLng(lat.toDouble(), lon.toDouble())))
-//                                }
-//                            }
-//                        }
-//                    }
-//
-//                })
-//                x++
-//                TimeUnit.SECONDS.sleep(5)
-//            }
-//        }).start()
+    /**
+     * Обновляет список транспорта онлайн на карте
+     * transportList - обновленный список транспорта онлайн
+     */
+    private fun updateTransportOnlineOnMap(transportList: List<TransportOnline>) {
+        mGoogleMap?.clear()
+        transportList.forEach {
+            val lat = "${it.lat.substring(0, 2)}.${it.lat.substring(2)}"
+            val lon = "${it.lon.substring(0, 2)}.${it.lon.substring(2)}"
+            mGoogleMap?.addMarker(MarkerOptions()
+                    .title(it.name)
+                    .snippet("Номер: ${it.num}")
+                    .position(LatLng(lat.toDouble(), lon.toDouble())))
+        }
     }
 }
--- app/src/main/java/ru/sergeykamyshov/rostovtransport/ui/online/TransportOnlineViewModel.kt
+++ app/src/main/java/ru/sergeykamyshov/rostovtransport/ui/online/TransportOnlineViewModel.kt
@@ -8,31 +8,69 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import ru.sergeykamyshov.rostovtransport.App
+import ru.sergeykamyshov.rostovtransport.data.network.OnlineRestService
 import ru.sergeykamyshov.rostovtransport.data.network.RestService
 import ru.sergeykamyshov.rostovtransport.data.network.model.online.Transport
+import ru.sergeykamyshov.rostovtransport.data.network.model.online.TransportOnline
+import java.util.concurrent.TimeUnit
 
 class TransportOnlineViewModel : ViewModel() {
 
+    private val mUpdateInterval: Long = 5
     val restService: RestService = App.createRestService()
-    private var mData = MutableLiveData<List<Transport.Item>>()
+    private var mTransportList = MutableLiveData<List<Transport.Item>>()
+    val onlineRestService: OnlineRestService = App.createOnlineRestService()
+    private var mTransportOnline = MutableLiveData<List<TransportOnline>>()
+    private var isRunning = true
 
-    fun getData(): LiveData<List<Transport.Item>> {
-        return mData
+    fun getTransportList(): LiveData<List<Transport.Item>> {
+        return mTransportList
     }
 
-    fun loadData() {
+    fun loadTransportList() {
         val call = restService.getTransportList()
         call.enqueue(object : Callback<Transport> {
             override fun onResponse(call: Call<Transport>?, response: Response<Transport>?) {
                 val transportList = response?.body()
-                Log.i("TOViewModel", "Last update: ${transportList?.lastUpdate}")
-                mData.postValue(transportList?.transport)
+                Log.i("TransportOnlineVM", "Last update: ${transportList?.lastUpdate}")
+                mTransportList.postValue(transportList?.transport)
             }
 
             override fun onFailure(call: Call<Transport>?, t: Throwable?) {
-                Log.i("TOViewModel", "Failed to get transport list: $t")
+                Log.i("TransportOnlineVM", "Failed to get transport list: $t")
             }
         })
+    }
+
+    fun getTransportOnline(): LiveData<List<TransportOnline>> {
+        return mTransportOnline
+    }
+
+    fun setRunning(isRunning: Boolean) {
+        this.isRunning = isRunning
+    }
+
+    fun loadTransportOnline(transportName: String) {
+        Thread(Runnable {
+            while (isRunning) {
+                val call = onlineRestService.getTransportByName(transportName)
+                call.enqueue(OnlineRestServiceCallback())
+
+                TimeUnit.SECONDS.sleep(mUpdateInterval)
+            }
+        }).start()
+    }
+
+    inner class OnlineRestServiceCallback : Callback<List<TransportOnline>> {
+        override fun onResponse(call: Call<List<TransportOnline>>?, response: Response<List<TransportOnline>>?) {
+            val transportOnline = response?.body()
+            Log.i("TransportOnlineVM", "Number of transport online: ${transportOnline?.size}")
+            mTransportOnline.postValue(transportOnline)
+        }
+
+        override fun onFailure(call: Call<List<TransportOnline>>?, t: Throwable?) {
+            Log.i("TransportOnlineVM", "Failed to get transport online: $t")
+        }
     }
 
 }
